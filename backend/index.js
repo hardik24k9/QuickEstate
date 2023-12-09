e641853668cd728a14602eeb50d8675c38ec0cc4
hardik24k9 <hardikpachori1383@gmail.com>
2023-12-10 02:26:43 +0530

    Added routes & controllers for auth

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+import userRouter from "./routes/user.js";
+import authRouter from "./routes/auth.js";
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -11,6 +14,21 @@ mongoose
   .catch((error) => console.log(error));
 
 const app = express();
+
+app.use(express.json());
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000!");

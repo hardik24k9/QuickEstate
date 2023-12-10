f595d73e780399084b4e4e69838895cc82ae3fb7
hardik24k9 <hardikpachori1383@gmail.com>
2023-12-10 05:37:33 +0530

    Completed the signin function

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +12,30 @@ export const signup = async (req, res, next) => {
     res.status(201).json("user created successfully.");
   } catch (error) {
     // console.log(error.message);
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    // firstly check if email is valid or not
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+
+    // if email is valid check for password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid password!"));
+
+    const { password: pass, ...validUserData } = validUser._doc;
+
+    // create jwt token for the authenticated user to store it in the browser cookie
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(validUserData);
+  } catch (error) {
     next(error);
   }
 };

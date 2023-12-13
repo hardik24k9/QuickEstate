20a98738d7a5c5b4a264998840ac5c34c4ff90d9
hardik24k9 <hardikpachori1383@gmail.com>
2023-12-13 14:06:07 +0530

    Added api route for update listing functionality

--- backend/routes/listing.js
+++ backend/routes/listing.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createListing, deleteListing } from "../controllers/listing.js";
+import {
+  createListing,
+  deleteListing,
+  updateListing,
+} from "../controllers/listing.js";
 import { verifyToken } from "../utils/validateToken.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;

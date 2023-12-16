33e6788af402c152b243e99c89899c4b9765b754
hardik24k9 <hardikpachori1383@gmail.com>
2023-12-16 08:14:33 +0530

    Added an api route for search functionality

--- backend/routes/listing.js
+++ backend/routes/listing.js
@@ -3,6 +3,7 @@ import {
   createListing,
   deleteListing,
   getListing,
+  getListings,
   updateListing,
 } from "../controllers/listing.js";
 import { verifyToken } from "../utils/validateToken.js";
@@ -13,5 +14,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/find/:id", getListing);
+router.get("/search", getListings);
 
 export default router;
